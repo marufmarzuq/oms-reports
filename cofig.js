5c1ff3abba398cf5ff31ca29fb05c7beb4cfc38b
marufmarzuq <marufmarzuq18@gmail.com>
2022-09-14 01:30:01 +0600

    Config file added

---
+++ cofig.js
@@ -0,0 +1,13 @@
+secret_key = 1234asdf
+
+PORT=4000
+
+################## Dev DB ###################
+DB_USER=postgres
+DB_HOST=34.133.41.129
+DB_DATABASE=postgres
+DB_PASSWORD=aqai@1b@2025
+DB_PORT=5432
+
+################## JWT AUTH ##################
+JWT_SECRET=AQAI2025
